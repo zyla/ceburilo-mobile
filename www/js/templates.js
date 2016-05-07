4add0185156875ba8bb6216e445dbc2e1078e2bc
Maciej Bielecki <zyla@prati.pl>
2016-05-07 15:03:41 +0200

    Refactor templates

--- www/js/templates.js
+++ www/js/templates.js
@@ -10,15 +10,25 @@
 
   templatesNode.parentNode.removeChild(templatesNode);
 
-  window.showTemplate = function showTemplate(name) {
+  window.copyTemplate = function copyTemplate(name) {
     var template = templates[name];
 
     if(!template) {
       throw new Error('Template not found: ' + name);
     }
+    
+    return template.cloneNode(true);
+  };
+
+  window.showTemplate = function showTemplate(name) {
+    var node = copyTemplate(name);
 
     var appNode = document.getElementById('app');
-    appNode.innerHTML = '';
-    appNode.appendChild(template.cloneNode(true));
-  }
+    clearNode(appNode);
+    appNode.appendChild(node);
+  };
+
+  window.clearNode = function clearNode(node) {
+    while(node.firstChild) { node.removeChild(node.firstChild); }
+  };
 })();
